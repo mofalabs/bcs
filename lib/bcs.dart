539165d1ccdef0214214e16a210452cbd35ce53b
0xmove <0xmovebuilder@gmail.com>
2023-10-02 11:01:52 +0800

    chore: rename

--- lib/bcs.dart
+++ lib/bcs.dart
@@ -2,7 +2,7 @@ library bcs;
 
 export 'package:bcs/index.dart';
 export 'package:bcs/bs58.dart';
-export 'package:bcs/legacy_registry.dart';
+export 'package:bcs/legacy_bcs.dart';
 export 'package:bcs/utils.dart';
 export 'package:bcs/uleb.dart';
 export 'package:bcs/writer.dart';
